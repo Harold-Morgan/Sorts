5ab7961bcd59e7c622aeafe1ec5a5e361744a620
Harold_Morgan <volik46@gmail.com>
2019-04-06 16:15:24 +0300

    Инициализирующие чейнджи

--- Sorts/Program.cs
+++ Sorts/Program.cs
@@ -8,8 +8,20 @@ namespace Sorts
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            int[] targetarray = new int[1000];
+            Random randNum = new Random();
+
+            for (int i = 0; i < targetarray.Length; i++)
+                targetarray[i] = randNum.Next(0, 1000);
+
+
+            Console.Write("[{0}]", string.Join(", ", targetarray));
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Sorts/Sorts/Sorter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts.Sorts
+{
+    //TODO: было бы прикольно зафигачить все по паттерну, но пока и так пойдёт
+    abstract class Sorter
+    {
+        public int[] SortedArray;
+
+        public int[] SelectionSort(int[] targetarray)
+        {
+            return targetarray;
+        }
+    }
+}
